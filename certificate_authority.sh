481d69dcbf79db9171cba98faadf865836ef6727
vanmanhpro <vanmanhpbc1998@gmail.com>
2019-03-21 09:01:59 +0700

    add all file's from work

---
+++ certificate_authority.sh
@@ -0,0 +1,276 @@
+#!/bin/bash
+
+# certificate authority certs
+cat > ca-config.json <<EOF
+{
+  "signing": {
+    "default": {
+      "expiry": "8760h"
+    },
+    "profiles": {
+      "kubernetes": {
+        "usages": ["signing", "key encipherment", "server auth", "client auth"],
+        "expiry": "8760h"
+      }
+    }
+  }
+}
+EOF
+
+cat > ca-csr.json <<EOF
+{
+  "CN": "Kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "VN",
+      "L": "Vinh",
+      "O": "Kubernetes",
+      "OU": "CA",
+      "ST": "NgheAn"
+    }
+  ]
+}
+EOF
+
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+
+# admin client certs
+cat > admin-csr.json <<EOF
+{
+  "CN": "admin",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "VN",
+      "L": "Vinh",
+      "O": "system:masters",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "NgheAn"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  admin-csr.json | cfssljson -bare admin
+
+# node certs
+for i in 0 1 2; do
+  instance="node-${i}"
+  instance_hostname="ip-10-240-0-2${i}"
+  cat > ${instance}-csr.json <<EOF
+{
+  "CN": "system:node:${instance_hostname}",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "VN",
+      "L": "Vinh",
+      "O": "system:nodes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "NgheAn"
+    }
+  ]
+}
+EOF
+
+  external_ip=$(aws ec2 describe-instances \
+    --filters "Name=tag:Name,Values=${instance}" \
+    --output text --query 'Reservations[].Instances[].PublicIpAddress')
+
+  internal_ip=$(aws ec2 describe-instances \
+    --filters "Name=tag:Name,Values=${instance}" \
+    --output text --query 'Reservations[].Instances[].PrivateIpAddress')
+
+  cfssl gencert \
+    -ca=ca.pem \
+    -ca-key=ca-key.pem \
+    -config=ca-config.json \
+    -hostname=${instance_hostname},${external_ip},${internal_ip} \
+    -profile=kubernetes \
+    node-${i}-csr.json | cfssljson -bare node-${i}
+done
+
+# kube controller manager cert
+
+cat > kube-controller-manager-csr.json <<EOF
+{
+  "CN": "system:kube-controller-manager",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "VN",
+      "L": "Vinh",
+      "O": "system:kube-controller-manager",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "NgheAn"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kube-controller-manager-csr.json | cfssljson -bare kube-controller-manager
+
+# kube proxy cert
+cat > kube-proxy-csr.json <<EOF
+{
+  "CN": "system:kube-proxy",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "VN",
+      "L": "Vinh",
+      "O": "system:node-proxier",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "NgheAn"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kube-proxy-csr.json | cfssljson -bare kube-proxy
+
+# scheduler client certs
+cat > kube-scheduler-csr.json <<EOF
+{
+  "CN": "system:kube-scheduler",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "VN",
+      "L": "Vinh",
+      "O": "system:kube-scheduler",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "NgheAn"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  kube-scheduler-csr.json | cfssljson -bare kube-scheduler
+
+# API server certs
+
+cat > kubernetes-csr.json <<EOF
+{
+  "CN": "kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "VN",
+      "L": "Vinh",
+      "O": "Kubernetes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "NgheAn"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -hostname=10.32.0.1,10.240.0.10,10.240.0.11,10.240.0.12,${KUBERNETES_PUBLIC_ADDRESS},127.0.0.1,kubernetes.default \
+  -profile=kubernetes \
+  kubernetes-csr.json | cfssljson -bare kubernetes
+
+# Service account keypair
+
+cat > service-account-csr.json <<EOF
+{
+  "CN": "service-accounts",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+  },
+  "names": [
+    {
+      "C": "VN",
+      "L": "Vinh",
+      "O": "Kubernetes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "NgheAn"
+    }
+  ]
+}
+EOF
+
+cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=kubernetes \
+  service-account-csr.json | cfssljson -bare service-account
+
+# copy certs to nodes
+# for instance in node-0 node-1 node-2; do
+#   external_ip=$(aws ec2 describe-instances \
+#     --filters "Name=tag:Name,Values=${instance}" \
+#     --output text --query 'Reservations[].Instances[].PublicIpAddress')
+
+#   scp -i kubernetes_hardway.id_rsa ca.pem ${instance}-key.pem ${instance}.pem ubuntu@${external_ip}:~/
+# done
+# replace with this, after you've filled in the config file
+for instance in node-0 node-1 node-2; do
+  scp ca.pem ${instance}-key.pem ${instance}.pem ${instance}:~/
+done
+
+
+# copy certs to masters
+# for instance in master-0 master-1 master-2; do
+#   external_ip=$(aws ec2 describe-instances \
+#     --filters "Name=tag:Name,Values=${instance}" \
+#     --output text --query 'Reservations[].Instances[].PublicIpAddress')
+
+#   scp -i kubernetes_hardway \
+#     ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem \
+#     service-account-key.pem service-account.pem ubuntu@${external_ip}:~/
+# done
+# replace with this, after you've filled in the config file
+for instance in master-0 master-1 master-2; do
+    scp \
+        ca.pem ca-key.pem kubernetes-key.pem kubernetes.pem \
+        service-account-key.pem service-account.pem ${instance}:~
+done
---
+++ resource.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+export VPC_ID=vpc-077cd03e6babbd4fe
+export SUBNET_ID=subnet-0a45109635d92968a
+export INTERNET_GATEWAY_ID=igw-03e4d730b47fcee72
+export ROUTE_TABLE_ID=rtb-0a3cc03e41816babe
+export SECURITY_GROUP_ID=sg-0b9dd928ff8cdb450
+export LOAD_BALANCER_ARN=arn:aws:elasticloadbalancing:ap-southeast-2:492995901256:loadbalancer/net/kubernetes/9b9525ff42b7f693
+export TARGET_GROUP_ARN=arn:aws:elasticloadbalancing:ap-southeast-2:492995901256:targetgroup/kubernetes/2ea8de7c6830c564
+export KUBERNETES_PUBLIC_ADDRESS=kubernetes-9b9525ff42b7f693.elb.ap-southeast-2.amazonaws.com
+export IMAGE_ID=ami-020c9730d79a10ec0
+export ENCRYPTION_KEY=fGW7+qUybROwZaf3fG8LWZ+f3LyzkvSnCPJ3TlsQrdg=
